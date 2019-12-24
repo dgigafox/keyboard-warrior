6cd498a39460d9bf2d2c454634a52dbb358ba38c
Darren Gegantino <darren.gegantino@martide.com>
2019-12-24 19:18:35 +0800

    Setup bootstrap and font-awesome

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -20,7 +20,10 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: "css/app.css",
+      order: {
+        after: ["priv/static/css/app.scss"] // concat app.css last
+      }
     },
     templates: {
       joinTo: "js/app.js"
@@ -37,7 +40,7 @@ exports.config = {
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
+    watched: ["static", "css", "js", "vendor", "scss", "fonts"],
     // Where to compile files to
     public: "../priv/static"
   },
@@ -47,6 +50,18 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
+    },
+    sass: {
+      mode: 'native',
+      options: {
+        includePaths: ["node_modules/font-awesome/scss", "node_modules/bootstrap/scss"], // Tell sass-brunch where to look for files to @import
+        precision: 8 // Minimum precision required by bootstrap-sass
+      }
+    },
+    copycat: {
+      "fonts": ["static/fonts", "node_modules/font-awesome/fonts"],
+      verbose: false, //shows each file that is copied to the destination directory
+      onlyChanged: true //only copy a file if it's modified time has changed (only effective when using brunch watch)
     }
   },
 
@@ -57,6 +72,13 @@ exports.config = {
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    globals: { // Bootstrap's JavaScript requires both '$' and 'jQuery' in global scope
+      $: 'jquery',
+      jQuery: 'jquery',
+      Tether: 'tether',
+      Popper: 'popper.js',
+      bootstrap: 'bootstrap', // Require Bootstrap's JavaScript globally
+    }
   }
 };
